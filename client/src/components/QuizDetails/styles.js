05ed8df9a3ccfc5172323eb8ef724cbe099437ae
plongtran239 <plongtran239@gmail.com>
2022-12-23 10:47:22 +0700

    Modify QuizDetails

--- client/src/components/QuizDetails/styles.js
+++ client/src/components/QuizDetails/styles.js
@@ -1,45 +1,45 @@
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  media: {
-    borderRadius: "20px",
-    objectFit: "cover",
-    width: "100%",
-    maxHeight: "400px",
-  },
-  card: {
-    display: "flex",
-    width: "100%",
-    [theme.breakpoints.down("sm")]: {
-      flexWrap: "wrap",
-      flexDirection: "column",
+    media: {
+        borderRadius: '20px',
+        objectFit: 'cover',
+        width: '40%',
+        maxHeight: '400px',
+        marginRight: '20px',
     },
-  },
-  section: {
-    borderRadius: "20px",
-    margin: "10px",
-    flex: 1,
-  },
-  imageSection: {
-    marginLeft: "20px",
-    width: "50%",
-    [theme.breakpoints.down("sm")]: {
-      marginLeft: 0,
+    card: {
+        display: 'flex',
+        flex: '1',
+        padding: '40px 20px',
+        [theme.breakpoints.down('sm')]: {
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+        },
     },
-  },
-  recommendedQuizes: {
-    display: "flex",
-    flexWrap: "wrap",
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
+    imageSection: {
+        // display: 'flex',
+        marginLeft: '20px',
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: 0,
+        },
     },
-  },
-  loadingPaper: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "20px",
-    borderRadius: "15px",
-    height: "100vh",
-  },
-}))
+    questions: {
+        flex: 1,
+    },
+    recommendedQuizes: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        [theme.breakpoints.down('sm')]: {
+            flexDirection: 'column',
+        },
+    },
+    loadingPaper: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '20px',
+        borderRadius: '15px',
+        height: '100vh',
+    },
+}));
